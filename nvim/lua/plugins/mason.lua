0ba65f4c35fb1f6a5428f653bd682d88600c41c4
DenisovichDev <bhaswarishi2908@gmail.com>
2026-07-29 22:02:34 +0000

    Intelliscneceeakbfe

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,36 @@
+return {
+    {
+        -- Install Mason
+        'mason-org/mason.nvim',
+        opts = {
+			ui = {
+				border = "rounded",
+				height = 0.85,
+				width = 0.8,
+			},
+		},
+    },
+    {
+		-- Bridge between mason.nvim and nvim with lspconfig
+		-- https://github.com/mason-org/mason-lspconfig.nvim
+        "mason-org/mason-lspconfig.nvim",
+        opts = {
+            ensure_installed = {
+                "lua_ls",
+                "clangd",
+                "jdtls",
+                "pyright",
+                "bashls",
+                "jsonls",
+                "html",
+                "cssls",
+                "ts_ls",
+            },
+        },
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
+    },
+
+}
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,62 @@
+return {
+    {
+        'nvim-treesitter/nvim-treesitter',
+        lazy = false,
+        build = ':TSUpdate',
+
+        config = function()
+            -- require('nvim-treesitter').setup({})
+
+            require('nvim-treesitter').install({
+                "lua",
+                "vim",
+                "vimdoc",
+                "query",
+
+                "bash",
+                "c",
+                "cpp",
+                "python",
+
+                "markdown",
+                "markdown_inline",
+
+                "json",
+                "yaml",
+                "toml",
+
+                "html",
+                "css",
+                "javascript",
+                "typescript",
+            })
+
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = {
+                    "lua",
+                    "vim",
+                    "c",
+                    "cpp",
+                    "python",
+                    "javascript",
+                    "typescript",
+                    "html",
+                    "css",
+                    "markdown",
+                    "json",
+                    "yaml",
+                    "toml",
+                    "bash",
+                },
+
+                callback = function()
+                    vim.treesitter.start()
+
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end,
+            })
+
+        end,
+
+    },
+}
