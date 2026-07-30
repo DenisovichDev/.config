acd73443c45a18a1764075b098f4426d1227e134
DenisovichDev <bhaswarishi2908@gmail.com>
2026-07-30 15:52:52 +0000

    Fixed cursor bug

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,30 +1,37 @@
+------------------------------------
+-- Neovim Config Files
+------------------------------------
+
+-- Copyright: DenisovichDev
+-- (https://denisovichdev.github.io/link-tree)
+
 local api = vim.api
 
 
 -- Numbermode Toggle
 local numtogglegrp = api.nvim_create_augroup("numbertoggle", { clear = true })
 api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
-   pattern = "*",
-   group = numtogglegrp,
-   callback = function()
-      if vim.o.nu and api.nvim_get_mode().mode ~= "i" then
-         vim.opt.relativenumber = true
-      end
-   end,
+    pattern = "*",
+    group = numtogglegrp,
+    callback = function()
+        if vim.o.nu and api.nvim_get_mode().mode ~= "i" then
+            vim.opt.relativenumber = true
+        end
+    end,
 })
 api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
-   pattern = "*",
-   group = numtogglegrp,
-   callback = function()
-      if vim.o.nu then
-         vim.opt.relativenumber = false
-         -- Conditional taken from https://github.com/rockyzhang24/dotfiles/commit/03dd14b5d43f812661b88c4660c03d714132abcf
-         -- Workaround for https://github.com/neovim/neovim/issues/32068
-         if not vim.tbl_contains({"@", "-"}, vim.v.event.cmdtype) then
-            vim.cmd "redraw"
-         end
-      end
-   end,
+    pattern = "*",
+    group = numtogglegrp,
+    callback = function()
+        if vim.o.nu then
+            vim.opt.relativenumber = false
+            -- Conditional taken from https://github.com/rockyzhang24/dotfiles/commit/03dd14b5d43f812661b88c4660c03d714132abcf
+            -- Workaround for https://github.com/neovim/neovim/issues/32068
+            if not vim.tbl_contains({ "@", "-" }, vim.v.event.cmdtype) then
+                vim.cmd "redraw"
+            end
+        end
+    end,
 })
 
 -- Highlight on yank
@@ -37,14 +44,14 @@ api.nvim_create_autocmd("TextYankPost", {
 -- Hide cursor when inactive
 local cursorgrp = api.nvim_create_augroup("CursorLine", { clear = true })
 api.nvim_create_autocmd(
-    {"WinEnter", "InsertEnter"}, {
+    { "WinEnter" }, {
         pattern = "*",
         command = "set cursorline",
         group = cursorgrp
     }
 )
 api.nvim_create_autocmd(
-    {"WinLeave", "InsertLeave"}, {
+    { "WinLeave" }, {
         pattern = "*",
         command = "set nocursorline",
         group = cursorgrp
