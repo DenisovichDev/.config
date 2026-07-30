3a34320a62a6571b4b64e63b89df584cf723930a
DenisovichDev <bhaswarishi2908@gmail.com>
2026-07-30 12:36:12 +0000

    Diagnostic Settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -20,38 +20,10 @@ require("config.keymaps")
 -- plugins
 require("config.lazy")
 
+-- diagnostics settings
+require("config.diagnostic")
 
 
---- nvim tree related, to be moved else where
- -- disable netrw at the very start of your init.lua
-  vim.g.loaded_netrw = 1
-  vim.g.loaded_netrwPlugin = 1
-
-  -- optionally enable 24-bit colour
-  -- vim.opt.termguicolors = true
-
-  -- empty setup using defaults
-  -- require("nvim-tree").setup()
-
-  -- OR setup with a config
-
-  ---@type nvim_tree.config
-
-  --   renderer = {
-  --     group_empty = true,
-  --   },
-  --   filters = {
-  --     dotfiles = true,
-  --   },
-  -- }
-  -- require("nvim-tree").setup(config)
--- ---
-
-
-
-
-
-print("Hello, Lua is running seccessfully. Everything is beautiful and nothing is hurt")
-
+print("Everything is beautiful and nothing is hurt")
 
 
